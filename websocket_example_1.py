8aae01e7df97df0ea60cfb5283664cd31617a3ea
Brian Hopkins <brianhh1230@gmail.com>
2018-06-12 08:19:52 -0400

    Revert "Renamed file and tweaked request"

--- websocket_example_1.py
+++ websocket_example_1.py
@@ -1,25 +1,21 @@
 from websocket import create_connection, WebSocket
 import ssl
 
-
 class MyWebSocket(WebSocket):
     def recv_message(self):
         message = super().recv_frame()
         print('Recieved message: {}'.format(message))
         return message
 
-ws = create_connection("ws://0.0.0.0:8181/core", \
-                       sslopt={"cert_reqs": ssl.CERT_NONE}, \
-                       class_=MyWebSocket)
 
 
-mycroft_request = 'what is the weather'
-
+ws = create_connection("ws://0.0.0.0:8181/core", sslopt={"cert_reqs": ssl.CERT_NONE}, class_=MyWebSocket)
+mycroft_question = 'what time is it'
 mycroft_type = 'recognizer_loop:utterance'
-mycroft_data = '{"utterances": ["%s"]}' % mycroft_request
+mycroft_data = '{"utterances": ["%s"]}' % mycroft_question
 message = '{"type": "' + mycroft_type + '", "data": ' + mycroft_data + '}'
-print("Sending request 'Message'...")
+print("Sending 'Message'...")
 ws.send(message)
 print(ws.recv())
 print(ws.recv())
-print("Request sent")
+print("Sent")
